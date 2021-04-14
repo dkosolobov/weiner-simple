f6459942f97ef44e573004c2e0c379344b4e35d0
dkosolobov <dkosolobov@mail.ru>
2021-04-15 02:06:49 +0500

    Changed len/pos to beg/end

--- main.cpp
+++ main.cpp
@@ -3,36 +3,36 @@
 
 const int MAXLEN = 1e6;
 std::string s;
-int pos[MAXLEN], len[MAXLEN], par[MAXLEN];
+int beg[MAXLEN], end[MAXLEN], par[MAXLEN];
 std::map<char, int> to[MAXLEN], link[MAXLEN];
 int n = 2;
 
-void attach(int kid, int node, char c, int ln) {
-	to[node][c] = kid;
-	len[kid] = ln;
+void attach(int kid, int node, char c, int b, int e) {
+	beg[kid] = b;
+	end[kid] = e;
 	par[kid] = node;
+	to[node][c] = kid;
 }
 
 void extend(char c) {
 	int p, i = s.size(), old = n - 1;
 	for (p = old; !link[p].count(c); p = par[p])
-		i -= len[p];
+		i -= end[p] - beg[p];
 	int v = link[p][c];
 	if (to[v].count(s[i])) {
-		int u = to[v][s[i]];
-		for (pos[n] = pos[u] - len[u]; s[pos[n]] == s[i]; pos[n] += len[p], i += len[p])
+		int j, u = to[v][s[i]];
+		for (j = beg[u]; s[j] == s[i]; j += end[p] - beg[p], i += end[p] - beg[p])
 			p = to[p][s[i]];
-		attach(n, v, s[pos[u] - len[u]], len[u] - (pos[u] - pos[n]));
-		attach(u, n, s[pos[n]], pos[u] - pos[n]);
+		attach(n, v, s[beg[u]], beg[u], j);
+		attach(u, n, s[j], j, end[u]);
 		v = link[p][c] = n++;
 	}
 	link[old][c] = n;
-	attach(n, v, s[i], s.size() - i);
-	pos[n++] = s.size();
+	attach(n++, v, s[i], i, s.size());
 }
 
 int main() {
-	len[1] = 1; pos[1] = 0; par[1] = 0;
+	beg[1] = 0; end[1] = 1; par[1] = 0;
 	for (int c = 0; c <= 255; c++)
 		to[0][c] = link[0][c] = 1;
 	s = "aabababbbbadcasdf#";
